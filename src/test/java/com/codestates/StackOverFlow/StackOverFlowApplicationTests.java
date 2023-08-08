858220f2c7637fb76ab907a5f41e6e0df96c54b5
uudean <swj4545@naver.com>
2023-08-08 11:07:15 +0900

    project init

---
+++ src/test/java/com/codestates/StackOverFlow/StackOverFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.StackOverFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
